655a32b07c7ed0617821a56eaea397e16b259c63
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-07-01 22:03:41 -0400

    feat login

--- src/modules/auth/component/login.js
+++ src/modules/auth/component/login.js
@@ -1,10 +1,35 @@
 import { useState } from "react"
-import { useHistory } from "react-router-dom"
+import { useHistory, Link } from "react-router-dom"
 
 function Login() {
-   return(
-       <p>login</p>
-   )
+
+
+    return (
+        <div className='container'>
+            <div className='div-login'>
+                <h4>Iniciar Sesion</h4>
+                <form onSubmit>
+                    <div>
+                        <p> Usuario </p>
+                        <input type='text' required></input>
+                    </div>
+                    <div>
+                        <p> Contraseña </p>
+                        <input type='password' required></input>
+                    </div>
+                    <div>
+                        <button type='submit'>Ingresar</button>
+                    </div>
+                    <div>
+                        <button type='submit'>Recuperar</button>
+                    </div>
+                </form>
+            </div >
+            <div className='img-container'>
+            <p></p>
+            </div>
+        </div>
+    )
 }
 
 export default Login
