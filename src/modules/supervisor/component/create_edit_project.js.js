dc66614c73ef6c3b596501256d3a72e82998ba58
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-07-07 22:55:09 -0400

    feat create edit project module component

--- src/modules/supervisor/component/create_edit_project.js.js
+++ src/modules/supervisor/component/create_edit_project.js.js
@@ -1,25 +1,39 @@
+
 const CreateEditProject = () => {
     return (
-        <>
-            <div>
-                <h2> Creacion de Proyecto</h2>
+        <section className='d-flex container section-project'>
+            <div className='w-50 container'>
                 <div>
-                    <form>
-                        <span>Nombre del Proyecto:</span>
-                        <input></input>
-                        <span>Nombre Supervisor:</span>
-                        <input></input>
-                        <p>Tecnicos Asignados al Proyecto:</p>
-                        <input></input>
-                        <p>Materiales a utilizar</p>
-                        <input></input>
-                        <p>Detalles del Proyecto</p>
-                        <input></input>
-                        <p> crear Proyecto</p>
-                    </form>
+                    <div className='input-add container'>
+                        <div className=' container border border-secondary'>
+                            <div className='text-align-center'>
+                                <h4>Add New Contact</h4>
+                            </div>
+                            <form className="form-horizontal" onSubmit>
+                                <label for="inputPassword" className="col-lg-2 control-label">Nombre del Proyecto</label>
+                                <input required type="text" className="form-control " placeholder="Nombre" aria-label="Recipient's username" onChange value />
+                                <label for="inputPassword" className="col-lg-2 control-label">Nombre del Supervisor</label>
+                                <input required type="text" className="form-control " placeholder="Email" aria-label="Recipient's username" onChange value />
+                                <label for="inputPassword" className="col-lg-2 control-label">Equipo</label>
+                                <input required type="text" className="form-control " placeholder="Telefono" aria-label="Recipient's username" onChange value />
+                                <label for="inputPassword" className="col-lg-2 control-label">Detalle del Proyecto</label>
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
+                                </div>
+                                <div>
+                                    <p>Materiales a Utilizar</p>
+                                    
+                                </div>
+                                <div className='m-top'>
+                                    <button>Crear</button>
+                                    <button>Cancelar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </>
+        </section >
     )
 }
 
