6180a0b5c29ec273be45d2d0b41d75d0c5987f80
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-07-13 21:30:59 -0400

    wip app manager view

---
+++ src/modules/app_manager/component/project_list.js
@@ -0,0 +1,35 @@
+const ProjectListAdmin = () => {
+    return (
+        <>
+            <div className='list-project'>
+                <div className='d-flex justify-content-between'>
+                    <div>
+                        <h2 className='text-center'>Proyectos Activos</h2>
+                    </div>
+                    <div className='d-flex'>
+                        <button> Crear Nuevo Usuario</button>
+                        <button>Inventario Existente</button>
+                    </div>
+                </div>
+                <hr />
+                <div className="d-flex justify-content-between border-bottom project-name">
+                    <div className='d-flex align-items-center'>
+                        <p>Proyecto 1</p>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between project-name">
+                    <div className='d-flex align-items-center'>
+                        <p>Proyecto 2</p>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between project-name">
+                    <div className='d-flex align-items-center'>
+                        <p>Proyecto 3</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectListAdmin
--- src/modules/app_manager/pages/app_manager.modules.js
+++ src/modules/app_manager/pages/app_manager.modules.js
@@ -1,6 +1,17 @@
-const AppManager=()=>{
-    return(
-<p>administrador de app</p>
+import ProjectListAdmin from "../component/project_list"
+import DashBoard from "../../../shared/dashboard"
+import { Route, Switch, useHistory } from "react-router-dom"
+
+const AppManager = () => {
+    return (
+
+        <DashBoard rol='appmanager'>
+            <Switch>
+                <Route path='/appmanager'>
+                    <ProjectListAdmin />
+                </Route>
+            </Switch>
+        </DashBoard>
     )
 }
 
